b796406756ad3ed3347e9757cd44723e0ef5c71f
sane5ever <sane5ever@gmail.com>
2019-03-29 20:55:16 +0400

    HW6: fix relative urls

--- src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
@@ -21,7 +21,7 @@ public class JspMealController extends AbstractMealController {
     @GetMapping("/delete")
     public String delete(HttpServletRequest request) {
         super.delete(getId(request));
-        return "redirect:meals";
+        return "redirect:/meals";
     }
 
     @GetMapping("/update")
@@ -48,7 +48,7 @@ public class JspMealController extends AbstractMealController {
         } else {
             super.update(meal, getId(request));
         }
-        return "redirect:meals";
+        return "redirect:/meals";
     }
 
     @PostMapping
