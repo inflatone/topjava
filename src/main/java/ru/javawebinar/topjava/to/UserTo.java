12cfc530eac16941586b9b7cda3ffcfe46c8a8de
sane5ever <sane5ever@gmail.com>
2019-04-27 08:20:22 +0400

    add auth via user service

--- src/main/java/ru/javawebinar/topjava/to/UserTo.java
+++ src/main/java/ru/javawebinar/topjava/to/UserTo.java
@@ -1,10 +1,16 @@
 package ru.javawebinar.topjava.to;
 
+import org.hibernate.validator.constraints.Range;
+import ru.javawebinar.topjava.util.UserUtil;
+
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.io.Serializable;
 
-public class UserTo extends BaseTo {
+public class UserTo extends BaseTo implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     @NotBlank
     @Size(min = 2, max = 100)
@@ -18,14 +24,19 @@ public class UserTo extends BaseTo {
     @Size(min = 5, max = 32, message = "length must between 5 and 32 characters")
     private String password;
 
+    @NotNull
+    @Range(min = 10, max = 10000)
+    private Integer caloriesPerDay = UserUtil.DEFAULT_CALORIES_PER_DAY;
+
     public UserTo() {
     }
 
-    public UserTo(Integer id, String name, String email, String password) {
+    public UserTo(Integer id, String name, String email, String password, int caloriesPerDay) {
         super(id);
         this.name = name;
         this.email = email;
         this.password = password;
+        this.caloriesPerDay = caloriesPerDay;
     }
 
     public String getName() {
@@ -52,13 +63,21 @@ public class UserTo extends BaseTo {
         this.password = password;
     }
 
+    public Integer getCaloriesPerDay() {
+        return caloriesPerDay;
+    }
+
+    public void setCaloriesPerDay(Integer caloriesPerDay) {
+        this.caloriesPerDay = caloriesPerDay;
+    }
+
     @Override
     public String toString() {
         return "UserTo{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
+                ", caloriesPerDay='" + caloriesPerDay + '\'' +
                 '}';
     }
 }
