73d2e2c73cf116c489eb001cae3ee6a5c9c9b3ec
sane5ever <sane5ever@gmail.com>
2019-05-05 19:44:36 +0400

    add exception handler controller + exception jsp

---
+++ src/main/java/ru/javawebinar/topjava/web/GlobalControllerExceptionHandler.java
@@ -0,0 +1,31 @@
+package ru.javawebinar.topjava.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+import ru.javawebinar.topjava.AuthorizedUser;
+import ru.javawebinar.topjava.util.ValidationUtil;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class GlobalControllerExceptionHandler {
+    private static final Logger log = LoggerFactory.getLogger(GlobalControllerExceptionHandler.class);
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView defaultErrorHandler(HttpServletRequest request, Exception e) throws Exception {
+        log.error("Exception at request " + request.getRequestURL(), e);
+        ModelAndView mav = new ModelAndView();
+        Throwable rootCause = ValidationUtil.getRootCause(e);
+        mav.addObject("exception", rootCause);
+        mav.addObject("message", rootCause.toString());
+        // Interceptor is not invoked, put userTo
+        AuthorizedUser user = SecurityUtil.safeGet();
+        if (user != null) {
+            mav.addObject("userTo", user.getUserTo());
+        }
+        return mav;
+    }
+}
