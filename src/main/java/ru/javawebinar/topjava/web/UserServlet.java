7ca46e68660a97a68b36cea9ce54b9f2c14cf863
sane5ever <sane5ever@gmail.com>
2019-06-08 07:31:17 +0400

    change forward to redirect in UserServlet

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 public class UserServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/users.jsp").forward(req, resp);
+//        req.getRequestDispatcher("/users.jsp").forward(req, resp);
+        resp.sendRedirect("users.jsp");
     }
 }
