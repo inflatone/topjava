c0805d571e7e7154ab9c7576bb5728df3c1ee97f
sane5ever <sane5ever@gmail.com>
2019-06-09 11:18:20 +0400

    add logging (slf4j & logback)

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -1,5 +1,8 @@
 package ru.javawebinar.topjava.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -7,8 +10,12 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class UserServlet extends HttpServlet {
+    private static final Logger log = LoggerFactory.getLogger(UserServlet.class);
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        log.debug("redirect to users");
+
 //        req.getRequestDispatcher("/users.jsp").forward(req, resp);
         resp.sendRedirect("users.jsp");
     }
