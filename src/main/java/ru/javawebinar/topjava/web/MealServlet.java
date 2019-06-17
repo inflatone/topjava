00d9c446203519db8d0df051230f4cd2fb622a3c
sane5ever <sane5ever@gmail.com>
2019-06-17 15:16:57 +0400

    add meal servlet (+ jsp rendering)

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -2,6 +2,7 @@ package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -9,12 +10,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class UserServlet extends HttpServlet {
-    private static final Logger log = LoggerFactory.getLogger(UserServlet.class);
+public class MealServlet extends HttpServlet {
+    private static final Logger log = LoggerFactory.getLogger(MealServlet.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        log.debug("forward to users");
-        req.getRequestDispatcher("/users.jsp").forward(req, resp);
+        log.info("getAll");
+        req.setAttribute("meals", MealsUtil.getWithExcess(MealsUtil.MEALS, MealsUtil.DEFAULT_CALORIES_PER_DAY));
+        req.getRequestDispatcher("/meals.jsp").forward(req, resp);
     }
 }
