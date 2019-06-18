6f0783c1a4640c7714e777f1e511d0da09b35144
sane5ever <sane5ever@gmail.com>
2019-06-18 13:59:38 +0400

    introduce spring dependency injection via xml

--- src/main/java/ru/javawebinar/topjava/SpringMain.java
+++ src/main/java/ru/javawebinar/topjava/SpringMain.java
@@ -2,9 +2,11 @@ package ru.javawebinar.topjava;
 
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.repository.UserRepository;
+import ru.javawebinar.topjava.service.UserService;
 
 import java.util.Arrays;
 import java.util.List;
@@ -16,11 +18,17 @@ public class SpringMain {
 
 //        UserRepository repository = (UserRepository) context.getBean("inmemoryUserRepository");
             UserRepository repository = context.getBean(UserRepository.class);
+
+            UserService service = context.getBean(UserService.class);
+            service.create(new User(null, "userName", "email@mail.ru", "password", Role.ROLE_ADMIN));
+            
             List<User> users = repository.getAll();
             System.out.println(users.size());
 
             MealRepository mealRepository = context.getBean(MealRepository.class);
             mealRepository.getAll().forEach(System.out::println);
+
+
         }
     }
 
