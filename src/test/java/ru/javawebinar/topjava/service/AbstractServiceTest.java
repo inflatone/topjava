d93cd2138b460af23dad47427cdcdc8f420a6165
sane5ever <sane5ever@gmail.com>
2019-08-03 07:54:13 +0400

    extract junit rules to single class

--- src/test/java/ru/javawebinar/topjava/service/AbstractServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractServiceTest.java
@@ -1,13 +1,11 @@
 package ru.javawebinar.topjava.service;
 
-import org.junit.AfterClass;
+import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.rules.ExpectedException;
+import org.junit.rules.ExternalResource;
 import org.junit.rules.Stopwatch;
-import org.junit.runner.Description;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
@@ -15,7 +13,8 @@ import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringRunner;
 import ru.javawebinar.topjava.ActiveDbProfileResolver;
 
-import java.util.concurrent.TimeUnit;
+import static ru.javawebinar.topjava.TimingRules.STOPWATCH;
+import static ru.javawebinar.topjava.TimingRules.SUMMARY;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration({
@@ -25,31 +24,12 @@ import java.util.concurrent.TimeUnit;
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
 @ActiveProfiles(resolver = ActiveDbProfileResolver.class)
 public abstract class AbstractServiceTest {
-    private static final Logger log = LoggerFactory.getLogger("result");
+    @ClassRule
+    public static ExternalResource summary = SUMMARY;
 
-    private static StringBuilder results = new StringBuilder();
-
-    // https://dzone.com/articles/applying-new-jdk-11-string-methods
-    private static String DELIM = "-".repeat(103);
+    @Rule
+    public Stopwatch stopwatch = STOPWATCH;
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
-
-    @Rule
-    // http://stackoverflow.com/questions/14892125/what-is-the-best-practice-to-determine-the-execution-time-of-the-bussiness-relev
-    public Stopwatch stopwatch = new Stopwatch() {
-        @Override
-        protected void finished(long nanos, Description description) {
-            String result = String.format("\n%-25s %7d", description.getMethodName(), TimeUnit.NANOSECONDS.toMicros(nanos));
-            results.append(result);
-            log.info(result + " ms\n");
-        }
-    };
-
-    @AfterClass
-    public static void printResult() {
-        log.info('\n' + DELIM +
-                "\nTest                                                                                       Duration, ms" +
-                '\n' + DELIM + '\n' + results + '\n' + DELIM + '\n');
-    }
 }
