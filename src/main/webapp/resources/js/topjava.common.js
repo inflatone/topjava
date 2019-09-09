c95dcd6986b723b8297ff61d741782789a6653fc
sane5ever <sane5ever@gmail.com>
2019-09-09 06:53:08 +0400

    add i18n on client side (ajax)

--- src/main/webapp/resources/js/topjava.common.js
+++ src/main/webapp/resources/js/topjava.common.js
@@ -11,11 +11,13 @@ function makeEditable(ctx) {
 }
 
 function add() {
+    $("#modalTitle").html(i18n["addTitle"]);
     form.find(":input").val("");
     $("#editRow").modal();
 }
 
 function updateRow(id) {
+    $("#modalTitle").html(i18n["editTitle"]);
     $.get(context.ajaxUrl + id, function (data) {
         $.each(data, function(key, value) {
             form.find("input[name='" + key + "']").val(value);
@@ -25,13 +27,13 @@ function updateRow(id) {
 }
 
 function deleteRow(id) {
-    if (confirm('Are you sure?')) {
+    if (confirm(i18n["common.confirm"])) {
         $.ajax({
             url: context.ajaxUrl + id,
             type: "DELETE"
         }).done(function () {
             context.updateTable();
-            successNoty("Deleted");
+            successNoty(i18n["common.deleted"]);
         });
     }
 }
@@ -48,7 +50,7 @@ function save() {
     }).done(function () {
         $("#editRow").modal("hide");
         context.updateTable();
-        successNoty("Saved");
+        successNoty(i18n["common.saved"]);
     });
 }
 
@@ -61,10 +63,10 @@ function closeNoty() {
     }
 }
 
-function successNoty(text) {
+function successNoty(key) {
     closeNoty();
     new Noty({
-        text: "<span class='fa fa-lg fa-check'></span> &nbsp;" + text,
+        text: "<span class='fa fa-lg fa-check'></span> &nbsp;" + i18n[key],
         type: 'success',
         layout: "bottomRight",
         timeout: 1000
@@ -74,7 +76,7 @@ function successNoty(text) {
 function failNoty(jqXHR) {
     closeNoty();
     failedNote = new Noty({
-        text: "<span class='fa fa-lg fa-exclamation-circle'></span> &nbsp;Error status: " + jqXHR.status + (jqXHR.responseJSON ? "<br>" + jqXHR.responseJSON : ""),
+        text: "<span class='fa fa-lg fa-exclamation-circle'></span> &nbsp;" + i18n["common.errorStatus"] + ": " + jqXHR.status + (jqXHR.responseJSON ? "<br>" + jqXHR.responseJSON : ""),
         type: 'error',
         layout: "bottomRight"
     }).show();
