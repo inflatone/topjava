6833bb6a69d2b78bb0ef0b132084cb46c50ed621
sane5ever <sane5ever@gmail.com>
2019-11-01 10:44:13 +0400

    HW2: inject spring context into meal servlet

--- src/main/java/ru/javaops/topjava/web/MealServlet.java
+++ src/main/java/ru/javaops/topjava/web/MealServlet.java
@@ -1,11 +1,10 @@
 package ru.javaops.topjava.web;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.util.StringUtils;
 import ru.javaops.topjava.model.Meal;
-import ru.javaops.topjava.repository.MealRepository;
-import ru.javaops.topjava.repository.inmemory.InMemoryMealRepository;
-import ru.javaops.topjava.util.MealsUtil;
+import ru.javaops.topjava.web.meal.MealRestController;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -19,29 +18,36 @@ import java.time.temporal.ChronoUnit;
 import java.util.Objects;
 
 public class MealServlet extends HttpServlet {
-    private static final Logger log = LoggerFactory.getLogger(MealServlet.class);
+    private ConfigurableApplicationContext springContext;
 
-    private MealRepository repository;
+    private MealRestController mealController;
 
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
-        repository = new InMemoryMealRepository();
+        springContext = new ClassPathXmlApplicationContext("spring/spring-app.xml");
+        mealController = springContext.getBean(MealRestController.class);
+    }
+
+    @Override
+    public void destroy() {
+        springContext.close();
+        super.destroy();
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         request.setCharacterEncoding(StandardCharsets.UTF_8.displayName());
-        String id = request.getParameter("id");
-
         Meal meal = new Meal(
-                id.isEmpty() ? null : Integer.valueOf(id),
                 LocalDateTime.parse(request.getParameter("dateTime")),
                 request.getParameter("description"),
                 Integer.valueOf(request.getParameter("calories"))
         );
-        log.info(meal.isNew() ? "Create {}" : "Update {}", meal);
-        repository.save(meal, SecurityUtil.authUserId());
+        if (StringUtils.isEmpty(request.getParameter("id"))) {
+            mealController.create(meal);
+        } else {
+            mealController.update(meal, getId(request));
+        }
         response.sendRedirect("meals");
     }
 
@@ -51,29 +57,27 @@ public class MealServlet extends HttpServlet {
         switch (action == null ? "all" : action) {
             case "delete":
                 int id = getId(request);
-                log.info("Delete {}", id);
-                repository.delete(id, SecurityUtil.authUserId());
+                mealController.delete(id);
                 response.sendRedirect("meals");
                 break;
             case "create":
             case "update":
                 final Meal meal = "create".equals(action)
                         ? new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000)
-                        : repository.get(getId(request), SecurityUtil.authUserId());
+                        : mealController.get(getId(request));
                 request.setAttribute("meal", meal);
                 request.getRequestDispatcher("/mealForm.jsp").forward(request, response);
                 break;
             case "all":
             default:
-                log.info("getAll");
-                request.setAttribute("meals", MealsUtil.getTOs(repository.getAll(SecurityUtil.authUserId()), SecurityUtil.authUserCaloriesPerDay()));
+                request.setAttribute("meals", mealController.getAll());
                 request.getRequestDispatcher("/meals.jsp").forward(request, response);
                 break;
         }
     }
 
-    private int getId(HttpServletRequest req) {
-        String paramId = Objects.requireNonNull(req.getParameter("id"));
+    private int getId(HttpServletRequest request) {
+        String paramId = Objects.requireNonNull(request.getParameter("id"));
         return Integer.parseInt(paramId);
     }
 }
