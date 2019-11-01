ad38fe7dbca1335190759c4fc2a207dbd2fd3a3f
sane5ever <sane5ever@gmail.com>
2019-11-01 11:15:39 +0400

    HW2: add user select feature

--- src/main/java/ru/javaops/topjava/web/SecurityUtil.java
+++ src/main/java/ru/javaops/topjava/web/SecurityUtil.java
@@ -3,8 +3,14 @@ package ru.javaops.topjava.web;
 import static ru.javaops.topjava.util.MealsUtil.DEFAULT_CALORIES_PER_DAY;
 
 public class SecurityUtil {
+    private static int id = 1;
+
     public static int authUserId() {
-        return 1;
+        return id;
+    }
+
+    public static void setAuthUserId(int id) {
+        SecurityUtil.id = id;
     }
 
     public static int authUserCaloriesPerDay() {
