010eb2a0cfb205d4cb54cdf77f811ac03844ea5b
sane5ever <sane5ever@gmail.com>
2019-11-22 08:13:24 +0400

    HW6: create meal jsp controller

--- src/main/java/ru/javaops/topjava/web/RootController.java
+++ src/main/java/ru/javaops/topjava/web/RootController.java
@@ -5,14 +5,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import ru.javaops.topjava.service.MealService;
 import ru.javaops.topjava.service.UserService;
+import ru.javaops.topjava.util.MealsUtil;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class RootController {
+    private final UserService userService;
+
+    private final MealService mealService;
+
     @Autowired
-    private UserService service;
+    public RootController(UserService userService, MealService mealService) {
+        this.userService = userService;
+        this.mealService = mealService;
+    }
 
     @GetMapping("/")
     public String root() {
@@ -21,7 +30,7 @@ public class RootController {
 
     @GetMapping("/users")
     public String getUsers(Model model) {
-        model.addAttribute("users", service.getAll());
+        model.addAttribute("users", userService.getAll());
         return "users";
     }
 
@@ -30,5 +39,11 @@ public class RootController {
         int userId = Integer.parseInt(request.getParameter("userId"));
         SecurityUtil.setAuthUserId(userId);
         return "redirect:meals";
+    }
+
+    @GetMapping("/meals")
+    public String getMeals(HttpServletRequest request, Model model) {
+        model.addAttribute("meals", MealsUtil.getTOs(mealService.getAll(SecurityUtil.authUserId()), SecurityUtil.authUserCaloriesPerDay()));
+        return "meals";
     }
 }
