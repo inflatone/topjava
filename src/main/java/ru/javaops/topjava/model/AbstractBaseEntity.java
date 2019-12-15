d83f6534f5017c053fb8967b873ea4af9473e0bb
sane5ever <sane5ever@gmail.com>
2019-12-15 10:15:33 +0400

    fix hibernate issue (redundant db sub-quiry)

--- src/main/java/ru/javaops/topjava/model/AbstractBaseEntity.java
+++ src/main/java/ru/javaops/topjava/model/AbstractBaseEntity.java
@@ -1,5 +1,7 @@
 package ru.javaops.topjava.model;
 
+import org.hibernate.Hibernate;
+
 import javax.persistence.*;
 
 // http://stackoverflow.com/questions/594597/hibernate-annotations-which-is-better-field-or-property-access
@@ -7,6 +9,8 @@ import javax.persistence.*;
 public class AbstractBaseEntity {
     public static final int START_SEQ = 100000;
 
+    // See https://hibernate.atlassian.net/browse/HHH-3718 and https://hibernate.atlassian.net/browse/HHH-12034
+    // Proxy initialization when accessing its identifier managed now by JPA_PROXY_COMPLIANCE setting
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "global_seq")
     @SequenceGenerator(name = "global_seq", sequenceName = "global_seq", allocationSize = 1, initialValue = START_SEQ)
@@ -36,7 +40,7 @@ public class AbstractBaseEntity {
         if (this == o) {
             return true;
         }
-        if (o == null || getClass() != o.getClass()) {
+        if (o == null || !getClass().equals(Hibernate.getClass(o))) {
             return false;
         }
         AbstractBaseEntity that = (AbstractBaseEntity) o;
