b123b457cb96329f2d89e86bb0b3740f12affea9
sane5ever <sane5ever@gmail.com>
2019-12-19 06:59:44 +0400

    add userTO validation

--- src/main/java/ru/javaops/topjava/web/user/AdminUIController.java
+++ src/main/java/ru/javaops/topjava/web/user/AdminUIController.java
@@ -3,12 +3,16 @@ package ru.javaops.topjava.web.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.javaops.topjava.model.User;
 import ru.javaops.topjava.service.UserService;
 import ru.javaops.topjava.to.UserTo;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.StringJoiner;
 
 @RestController
 @RequestMapping("/ajax/admin/users")
@@ -39,12 +43,28 @@ public class AdminUIController extends AbstractUserController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void createOrUpdate(UserTo userTo) {
+    public ResponseEntity<String> createOrUpdate(@Valid UserTo userTo, BindingResult result) {
+        return result.hasErrors() ? composeUnprocessableEntity(result) : createOrUpdate(userTo);
+    }
+
+    private ResponseEntity<String> createOrUpdate(UserTo userTo) {
         if (userTo.isNew()) {
             super.create(userTo);
         } else {
             super.update(userTo, userTo.id());
         }
+        return ResponseEntity.ok().build();
+    }
+
+    private ResponseEntity<String> composeUnprocessableEntity(BindingResult result) {
+        var joiner = new StringJoiner("<br>");
+        result.getFieldErrors().forEach(e -> {
+            var message = e.getDefaultMessage();
+            if (message != null) {
+                joiner.add(message.startsWith(e.getField()) ? message : (e.getField() + ' ' + message));
+            }
+        });
+        return ResponseEntity.unprocessableEntity().body(joiner.toString());
     }
 
     @Override
