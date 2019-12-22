09ea5255fdcc53ee23ef67daa7dbf69ae63cd70c
sane5ever <sane5ever@gmail.com>
2019-12-22 16:33:15 +0400

    HW5: implement data jpa meal repository

--- src/main/java/ru/javaops/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javaops/topjava/repository/datajpa/CrudMealRepository.java
@@ -1,8 +1,28 @@
 package ru.javaops.topjava.repository.datajpa;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javaops.topjava.model.Meal;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Transactional(readOnly = true)
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
+    @Modifying
+    @Transactional
+    int delete(@Param("id") int id, @Param("userId") int userId);
+
+    @Override
+    @Transactional
+    Meal save(Meal meal);
+
+    List<Meal> getAll(@Param("userId") int userId);
+
+    @SuppressWarnings("JpaQIInspection")
+    List<Meal> getBetween(
+            @Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate,
+            @Param("userId") int userId);
 }
