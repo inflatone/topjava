2e7da856269c080cb47fe84ef484096d2aa49e49
sane5ever <sane5ever@gmail.com>
2019-12-22 17:43:02 +0400

    HW5: extract timing test rules

--- src/test/java/ru/javaops/topjava/service/AbstractServiceTest.java
+++ src/test/java/ru/javaops/topjava/service/AbstractServiceTest.java
@@ -1,22 +1,18 @@
 package ru.javaops.topjava.service;
 
-import org.junit.AfterClass;
+import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.rules.ExpectedException;
+import org.junit.rules.ExternalResource;
 import org.junit.rules.Stopwatch;
-import org.junit.runner.Description;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringRunner;
 import ru.javaops.topjava.ActiveDbProfileResolver;
-
-import static java.lang.String.format;
-import static java.util.concurrent.TimeUnit.NANOSECONDS;
-import static org.slf4j.LoggerFactory.getLogger;
+import ru.javaops.topjava.TimingRules;
 
 @RunWith(SpringRunner.class)
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
@@ -25,33 +21,12 @@ import static org.slf4j.LoggerFactory.getLogger;
         "classpath:spring/spring-db.xml"})
 @ActiveProfiles(resolver = ActiveDbProfileResolver.class)
 public abstract class AbstractServiceTest {
-    // https://dzone.com/articles/applying-new-jdk-11-string-methods
-    private static final String DELIM = "-".repeat(103);
+    @ClassRule
+    public static ExternalResource summary = TimingRules.SUMMARY;
 
-    private static final Logger log = getLogger("result");
-
-    private static StringBuilder results = new StringBuilder();
+    @Rule
+    public Stopwatch stopwatch = TimingRules.STOPWATCH;
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
-
-    // http://stackoverflow.com/questions/14892125/what-is-the-best-practice-to-determine-the-execution-time-of-the-bussiness-relev
-    @Rule
-    public Stopwatch stopwatch = new Stopwatch() {
-        @Override
-        protected void finished(long nanos, Description description) {
-            var result = format("%-95s %7d", description.getMethodName(), NANOSECONDS.toMillis(nanos));
-            results.append(result).append('\n');
-            log.info(result + " ms\n");
-        }
-    };
-
-    @AfterClass
-    public static void printResults() {
-        log.info('\n' + DELIM
-                + "\nTest" + " ".repeat(86) + "Durations, ms"
-                + '\n' + DELIM + '\n' + results + DELIM + '\n'
-        );
-        results.setLength(0);
-    }
 }
