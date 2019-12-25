b9d07d4a290db7123927085e54e57f4fc93179b4
sane5ever <sane5ever@gmail.com>
2019-12-25 10:14:41 +0400

    cascade + ddl generation example

--- src/main/java/ru/javaops/topjava/model/AbstractBaseEntity.java
+++ src/main/java/ru/javaops/topjava/model/AbstractBaseEntity.java
@@ -15,6 +15,7 @@ public class AbstractBaseEntity implements Persistable<Integer> {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "global_seq")
     @SequenceGenerator(name = "global_seq", sequenceName = "global_seq", allocationSize = 1, initialValue = START_SEQ)
+//    @Column(name = "id", unique = true, nullable = false, columnDefinition = "integer default nextval('global_seq')")
     protected Integer id;
 
     protected AbstractBaseEntity() {
