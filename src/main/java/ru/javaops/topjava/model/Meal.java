955184fa9721b2927bcd4c9cd0987e4d0aeb127e
sane5ever <sane5ever@gmail.com>
2020-01-03 08:02:14 +0400

    set force user existence validation  before save/update

--- src/main/java/ru/javaops/topjava/model/Meal.java
+++ src/main/java/ru/javaops/topjava/model/Meal.java
@@ -4,6 +4,7 @@ import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 import org.hibernate.validator.constraints.Range;
 import org.springframework.format.annotation.DateTimeFormat;
+import ru.javaops.topjava.View.Persist;
 import ru.javaops.topjava.util.DateTimeUtil;
 
 import javax.persistence.*;
@@ -46,6 +47,7 @@ public class Meal extends AbstractBaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
+    @NotNull(groups = Persist.class)
     private User user;
 
     public Meal() {
