30b53591adbd31bbdb94dbf2c4037121403a39ef
sane5ever <sane5ever@gmail.com>
2020-01-04 07:54:48 +0400

    add spring security taglibs + annotations

--- src/main/java/ru/javaops/topjava/web/RootController.java
+++ src/main/java/ru/javaops/topjava/web/RootController.java
@@ -1,5 +1,6 @@
 package ru.javaops.topjava.web;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -10,6 +11,8 @@ public class RootController {
         return "redirect:meals";
     }
 
+    // @Secured("ROLE_ADMIN")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("/users")
     public String getUsers() {
         return "users";
