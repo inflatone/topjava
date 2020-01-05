7c9e9b2bf293f5e3d4eff9db61ec58894347467d
sane5ever <sane5ever@gmail.com>
2020-01-05 10:36:12 +0400

    add json jackson

---
+++ src/test/java/ru/javaops/topjava/web/user/AdminRestControllerTest.java
@@ -0,0 +1,23 @@
+package ru.javaops.topjava.web.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import ru.javaops.topjava.web.AbstractControllerTest;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static ru.javaops.topjava.UserTestData.ADMIN_ID;
+
+class AdminRestControllerTest extends AbstractControllerTest {
+    private static final String REST_URL = AdminRestController.REST_URL + '/';
+
+    @Test
+    void getAdmin() throws Exception {
+        mockMvc.perform(get(REST_URL + ADMIN_ID))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+    }
+}
