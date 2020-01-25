c57caf63abd3b27ebffaf12056106ed49a780789
sane5ever <sane5ever@gmail.com>
2020-01-25 06:00:00 +0400

    heroku deployed

--- src/test/java/ru/javaops/topjava/web/user/HerokuAdminRestControllerTest.java
+++ src/test/java/ru/javaops/topjava/web/user/HerokuAdminRestControllerTest.java
@@ -1,10 +1,16 @@
 package ru.javaops.topjava.web.user;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.ResourcePropertySource;
 import org.springframework.test.context.ActiveProfiles;
 import ru.javaops.topjava.util.exeption.ErrorType;
 import ru.javaops.topjava.web.AbstractControllerTest;
 
+import java.io.IOException;
+
+import static java.util.Objects.requireNonNull;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static ru.javaops.topjava.Profiles.HEROKU;
@@ -13,6 +19,22 @@ import static ru.javaops.topjava.util.exeption.ModificationRestrictionException.
 
 @ActiveProfiles({HEROKU})
 public class HerokuAdminRestControllerTest extends AbstractControllerTest {
+
+    static {
+        Resource resource = new ClassPathResource("db/postgres.properties");
+        try {
+            var propertySource = new ResourcePropertySource(resource);
+            var herokuDbUrl = String.format("postgres://%s:%s@%s",
+                    propertySource.getProperty("database.username"),
+                    propertySource.getProperty("database.password"),
+                    ((String) requireNonNull(propertySource.getProperty("database.url"), "url must be set")).substring(18));
+            System.out.println(herokuDbUrl);
+            System.setProperty("DATABASE_URL", herokuDbUrl);
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
     public HerokuAdminRestControllerTest() {
         super(AdminRestController.REST_URL);
     }
